28cad07ac75520ed186d280b11f9435b0a890f04
ellmo <ellmunzai@gmail.com>
2014-03-18 14:42:46 +0100

    reversed map (0,0 is now SW corner)

--- app.rb
+++ app.rb
@@ -8,11 +8,11 @@ ERROR_STRINGS = File.read('error_strings.txt').split("\n")
 def redraw
   system("clear") || system("cls")
   puts " #{' ' * 15} [P]LACE {x} {y}"
-  puts "#{@board.draw 0}  [M]OVE"
-  puts "#{@board.draw 1}  [L]EFT"
+  puts "#{@board.draw 4}  [M]OVE"
+  puts "#{@board.draw 3}  [L]EFT"
   puts "#{@board.draw 2}  [R]IGHT"
-  puts "#{@board.draw 3}  R[E]PORT"
-  puts "#{@board.draw 4}  [Q]UIT"
+  puts "#{@board.draw 1}  R[E]PORT"
+  puts "#{@board.draw 0}  [Q]UIT"
   puts "#{@error}"
   print '>> '
 end
